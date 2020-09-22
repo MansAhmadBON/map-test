c2b1cb4df36acd55fb4a88feb12d442996b60f2e
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2020-09-22 17:19:32 +0300

    feat: solution of the task

---
+++ precache-manifest.1bd9a6b6451e5b95313cf8e386304a0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39bfed36f82643ee36dc19700ffe77a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7af7ae1c0689b586391a",
+    "url": "/static/css/main.799d379b.chunk.css"
+  },
+  {
+    "revision": "09b799e8fbe4450853f1",
+    "url": "/static/js/2.432ddfd3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.432ddfd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7af7ae1c0689b586391a",
+    "url": "/static/js/main.fde23b47.chunk.js"
+  },
+  {
+    "revision": "25ea74c6eeb94d6becad",
+    "url": "/static/js/runtime-main.4b9e0a0f.js"
+  }
+]);
